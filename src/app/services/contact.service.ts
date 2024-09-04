60eaadd1e64f67f7dbcd0ddb946d55bf4cb5d4f0
rodr1alex <rodr1alex27@gmail.com>
2024-09-03 20:37:07 -0400

    cors1

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -7,11 +7,28 @@ import { Observable, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class ContactService {
-  url: string ='http://18.231.193.172:8081/enviar'
-  constructor(private http: HttpClient) { }
+  url: string ='http://18.231.193.172/enviar'
+  // constructor(private http: HttpClient) { }
 
-  sendMessage(message: Message): Observable<any>{
-    return this.http.post<any>(`${this.url}`, message);
+  // sendMessage(message: Message): Observable<any>{
+  //   return this.http.post<any>(`${this.url}`, message);
+  // }
+
+  sendMessage(message: any): Promise<void> {
+    return fetch(this.url, {
+      method: 'POST',
+      mode: 'no-cors', // Importante: esto es lo que limita el acceso a la respuesta
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(message)
+    })
+    .then(() => {
+      console.log('Message sent with no-cors mode');
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+    });
   }
 
 }
