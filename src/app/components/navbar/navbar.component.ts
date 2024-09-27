f059fa6342d2f1a5337a04142b6cf91244d06601
roro <rodr1alex27@gmail.com>
2024-09-26 21:41:49 -0300

    navbar con responsivo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { flush } from '@angular/core/testing';
 
 @Component({
   selector: 'navbar',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  ngOnInit(): void {
+    this.showMenu()
+  }
+  menuToggle: boolean = false;
 
+
+  showMenu(){
+    const menu = document.getElementById('menu');
+    const iconToggle = document.getElementById('iconToggle');
+    console.log(menu)
+    this.menuToggle === true ? 
+      (this.menuToggle = false, menu?.classList.add('menu-visible'), iconToggle?.classList.add('fa-close')):
+      (this.menuToggle = true, menu?.classList.remove('menu-visible'), iconToggle?.classList.remove('fa-close'))
+  }
 }
