8d2c38d125ecece4f5a91f7de409acf5dcd66ed4
roro <rodr1alex27@gmail.com>
2024-09-26 22:37:44 -0300

    mejora en navbar, agrega footer

--- src/app/components/porfolio/porfolio.component.ts
+++ src/app/components/porfolio/porfolio.component.ts
@@ -5,11 +5,12 @@ import { ProyectCardComponent } from '../proyect-card/proyect-card.component';
 import { SkillsComponent } from "../skills/skills.component";
 import { ContactFormComponent } from "../contact-form/contact-form.component";
 import { ProyectsComponent } from "../proyects/proyects.component";
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'porfolio',
   standalone: true,
-  imports: [NavbarComponent, AboutComponent, ProyectCardComponent, SkillsComponent, ContactFormComponent, ProyectsComponent],
+  imports: [NavbarComponent, AboutComponent, ProyectCardComponent, SkillsComponent, ContactFormComponent, ProyectsComponent, FooterComponent],
   templateUrl: './porfolio.component.html'
 })
 export class PorfolioComponent {
